e071f0736ecd38d49dc88fa61979ed5686d48b72
tornsheetmetal <tornsheetmetal>
2001-12-20 19:41:00 +0000

    added line2word func

--- gdspsim/decode_window.h
+++ gdspsim/decode_window.h
@@ -27,6 +27,8 @@ GtkWidget *create_decode_window();
 void highlight_decode_index(unsigned long int index);
 void set_decode_index_max_range(unsigned long int lower_index, unsigned long int upper_index);
 void highlight_pipeline(WordA prefetch);
+// Convert a line number to the corresponding word
+WordA line2word(GArray *word2line, int line, WordA start);
 
 
 #endif // __DECODE_WINDOW_H__
