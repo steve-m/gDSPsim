5807639de432924a84a689b5dc513ad51d897c32
tornsheetmetal <tornsheetmetal>
2002-02-21 04:11:01 +0000

    execute stage was blank

--- gdspsim/abdst.c
+++ gdspsim/abdst.c
@@ -17,9 +17,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+// Audit: Feb 20,2002:1 wkk
+
 #include "c54_core.h"
-#include "hardware.h"
-#include <stdio.h>
 #include "xymem.h"
 
 static GPtrArray *machine_code(gchar *opcode_text);
@@ -53,6 +53,86 @@ Instruction_Class ABDST_Obj =
 
 static void execute(struct _PipeLine *pipeP, struct _Registers *Reg)
 {
+  // much taken from ALU, abs properties and FRCT reasons for embedding
+  union _GP_Reg_Union X,Y;
+
+  // B = B + |A[32-16]|
+  X.gint64 = 0;
+  Y.gint64 = 0;
+
+  X.guint64 = GP_REG17_TO_UINT64(MMR->A);
+  if ( X.gint64 < 0 )
+    X.gint64 = -X.gint64;
+
+  Y.guint64 = GP_REG_2_UINT64(MMR->B);
+
+  X.gint64 = X.gint64 + Y.gint64;
+
+  // Set C bit. FIXME? just guessing on how the C is set
+  if ( X.guint64 & (guint64)0x100000000 )
+    set_C(MMR,1);
+
+  // Check for overflow and set overflow bit
+  if ( X.gint64 > max_pos32 )
+    {
+      set_OVB(MMR,1);
+      if ( OVM(MMR) )
+	X.gint64 = max_pos32;
+    }
+  else if ( X.gint64 < max_neg32 )
+    {
+      set_OVB(MMR,1);
+      if ( OVM(MMR) )
+	X.gint64 = max_neg32;
+    }
+
+  MMR->B = X.gp_reg;
+
+  // A=|Xmem-Ymem|<<16  
+
+  X.gint64 = 0;
+  Y.gint64 = 0;
+
+  if ( SXM(MMR) )
+    X.gint64 = (SWord)Reg->DB;
+  else
+    X.words.low = Reg->DB;
+  
+  if ( SXM(MMR) )
+    Y.gint64 = (SWord)Reg->CB;
+  else
+    Y.words.low = Reg->CB;
+  
+  X.gint64 = X.gint64 - Y.gint64;
+  
+  if ( X.gint64 < 0 )
+    X.gint64 = -X.gint64;
+
+  X.guint64 = X.guint64 << 16;
+
+  if ( FRCT(MMR) )
+    X.guint64 = X.guint64 << 1;
+
+  // Set C bit. FIXME? just guessing on how the C is set
+  if ( X.guint64 & (guint64)0x100000000 )
+    set_C(MMR,1);
+
+  // Check for overflow and set overflow bit
+  if ( X.gint64 > max_pos32 )
+    {
+      set_OVA(MMR,1);
+      if ( OVM(MMR) )
+	X.gint64 = max_pos32;
+    }
+  else if ( X.gint64 < max_neg32 )
+    {
+      set_OVA(MMR,1);
+      if ( OVM(MMR) )
+	X.gint64 = max_neg32;
+    }
+
+  MMR->A = X.gp_reg;
+
 }
 
 
