0ac8f5347c2228827b0b943868bae8e4044cf554
tornsheetmetal <tornsheetmetal>
2002-02-23 21:46:11 +0000

    added symbol decoding and stall

--- gdspsim/bc.c
+++ gdspsim/bc.c
@@ -17,6 +17,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+// Audit: Feb 23,2002:1 wkk
+
 #include "c54_core.h"
 #include "hardware.h"
 #include <stdio.h>
@@ -26,9 +28,9 @@
 static void read_stg1(struct _PipeLine *pipeP, struct _Registers *Reg);
 static GPtrArray *machine_code(gchar *opcode_text);
 
-static gchar *mask[]=    { "111110z0 cccccccc hhhhhhhh hhhhhhhh" };
-static gchar *opcode[] = { "BCz h,c" };
-static gchar *comment[]= { "Branch $(z) $(h),$(c)" };
+static gchar *mask[]=    { "111110z0 cccccccc llllllll llllllll" };
+static gchar *opcode[] = { "BCz l,c" };
+static gchar *comment[]= { "Branch $(z) $(l),$(c)" };
 
 /* This definition is global because another routine will make have
  * an array that points to all the different instruction classes.
@@ -53,19 +55,25 @@ Instruction_Class BC_Obj =
 
 static void read_stg1(struct _PipeLine *pipeP, struct _Registers *Reg)
 {
-  if ( pipeP->word_number == 1 )
+  // stall by 1 cycle
+  if ( pipeP->cycles == 0 )
+    Reg->Decode_Again = 1;
+
+  if ( pipeP->cycles == 1 )
     {
-      // Check condition codes
-      if ( check_condition(pipeP->current_opcode & 0xff) )
+      if ( pipeP->word_number == 1 )
 	{
-	  Reg->PC = Reg->IR;
-	  
-	  if ( (pipeP->current_opcode & 0x200) == 0 )
+	  // Check condition codes
+	  if ( check_condition(pipeP->current_opcode & 0xff) )
 	    {
-	      Reg->Flush = Reg->Flush + 2;
+	      Reg->PC = Reg->IR;
+	      
+	      if ( (pipeP->current_opcode & 0x200) == 0 )
+		{
+		  Reg->Flush = Reg->Flush + 2;
+		}
 	    }
-       	}
-      
+	}
     }
 }
 
