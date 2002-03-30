ecaff63f2578052e5e841f0053b981698f55625c
tornsheetmetal <tornsheetmetal>
2002-03-30 07:14:34 +0000

    header file for portability

---
+++ gdspsim/chip_core.h
@@ -0,0 +1,31 @@
+/*
+ * gDSPsim - GNU Digital Signal Processor Simulator
+ *
+ * Copyright (C) 2001-2002, Kerry Keal, kerry@industrialmusic.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2, as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef __CHIP_CORE_H__
+#define __CHIP_CORE_H__
+
+#ifdef _C55
+#include "c55_core.h"
+#endif
+#ifdef _C54
+#include "c54_core.h"
+#endif
+
+
+#endif // __CHIP_CORE_H__
