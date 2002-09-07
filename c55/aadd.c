d3a6cede5fcf5fad8342cd844bb06b0c15573b21
tornsheetmetal <tornsheetmetal>
2002-09-07 17:22:33 +0000

    added 4 more incarnations and execute stage for all

--- c55/aadd.c
+++ c55/aadd.c
@@ -17,15 +17,27 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
       
-#include "chip_core.h"
-#include "smem.h"
+#include <chip_core.h>
+#include <smem.h>
+#include <alu.h>
+#include <chip_help.h>
+
 static gchar *mask[]=
-  {
-    "0100 1110 nnnn nnnn", // AADD #n,SP
-  };
+{
+  "0001010p rrrrvvvv RRRR0000", // AADD TAx, TAy  (prefered)
+  "0001010p nnnnnnnn RRRR0100", // AADD k8, TAx   (prefered)
+  "0001010p rrrrvvvv RRRR1000", // AADD TAx, TAy
+  "0001010p nnnnnnnn RRRR1100", // AADD k8, TAx
+  "0100111p nnnnnnnn", // AADD #n,SP
+};
+
 static gchar *opcode[] = 
 { 
-  "AADD #n,SP",
+  "'AADD' r,R",
+  "'AADD' #n,R",
+  "'AADD' r,R",
+  "'AADD' #n,R",
+  "'AADD' #n,'SP'",
 };
 
 static void execute(struct _PipeLine *pipeP, struct _Registers *Reg);
@@ -41,7 +53,7 @@ Instruction_Class AADD_Obj =
   execute, // execute
   NULL, // write 
   NULL, // write_plus
-  1,
+  5,
   mask,
   opcode,
 };
@@ -50,6 +62,62 @@ Instruction_Class AADD_Obj =
 
 static void execute(struct _PipeLine *pipeP, struct _Registers *Reg)
 {
+  Opcode opcode;
+  int r,R;
+  int k;
+  SWord n;
+  union _GP_Reg_Union reg_union1;
+  union _GP_Reg_Union reg_union2;
+
+  opcode = pipeP->decode_nfo.mach_code;
+
+  switch ( pipeP->opcode_subType )
+    {
+    case 0:
+    case 2:
+      // AADD TAx, TAy
+      r = (opcode.bop[1]>>4)&0xf;
+      R = (opcode.bop[2]>>4)&0xf;
+
+      reg_union1 = get_register(r,1);
+      reg_union2 = get_register(R,1);
+
+      if ( (R>7) && ARLC(MMR,R) )
+	{
+	  reg_union2.gint64 = circular_update(reg_union2.words.low, R, (SWord)reg_union1.words.low);
+	}
+      else
+	{
+	  reg_union2.gint64 = reg_union2.gint64 + reg_union1.gint64;
+	}
+
+      set_register(reg_union2,R);
+      return;
+
+    case 1:
+    case 3:
+      // AADD k8, TAx
+      n = (char)opcode.bop[1];
+      R = (opcode.bop[2]>>4)&0xf;
+
+      reg_union2 = get_register(R,1);
+
+      if ( (R>7) && ARLC(MMR,R) )
+	{
+	  reg_union2.gint64 = circular_update(reg_union2.words.low, R, n);
+	}
+      else
+	{
+	  reg_union2.gint64 = reg_union2.gint64 + n;
+	}
+
+      set_register(reg_union2,R);
+      return;
+    case 4:
+      k = (char)opcode.bop[1];
+      MMR->SP += k;
+      return;
+    }
 }
 
 
